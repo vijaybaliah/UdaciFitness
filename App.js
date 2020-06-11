7e4bf6c85d2695c35798922aa04ea73eaa7977d2
vijaybaliah <vijaybalu1989@gmail.com>
2020-06-11 14:35:22 +0530

    feat: added initial changes

--- App.js
+++ App.js
@@ -1,19 +1,102 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Platform, StatusBar } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+import Constants from 'expo-constants';
 
-export default function App() {
+import reducer from './reducers';
+import History from './components/History';
+import AddEntry from './components/AddEntry';
+import { white, purple } from './utils/colors';
+import EntryDetail from './components/EntryDetail';
+import Live from './components/Live';
+import { setLocalNotification } from './utils/helpers'
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const Tabs = {
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    },
   },
-});
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    },
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
+    }
+  }
+}
+
+const navigationOptions = {
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+}
+
+const TabNav = Platform.OS === 'ios' ? createBottomTabNavigator(Tabs, navigationOptions) : createMaterialTopTabNavigator(Tabs, navigationOptions)
+
+const MainNavigator = createAppContainer(createStackNavigator({
+  Home: {
+    screen: TabNav,
+    navigationOptions: ({navigation}) => ({
+      header: null,
+    }),
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: ({navigation}) => ({
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      }
+    })
+  }
+}))
+
+export default class App extends Component {
+  componentDidMount() {
+    setLocalNotification()
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+          <MainNavigator />
+        </View>
+      </Provider>
+    );
+  }
+}
