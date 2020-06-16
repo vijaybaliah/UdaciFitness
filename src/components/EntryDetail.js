745ae0cce7a1be0ee6c5c9ae0da9a308a3e03ef3
Praveen_Chinniah <praveenchinniah25@gmail.com>
2020-06-17 01:56:43 +0530

    textInput and button

--- src/components/EntryDetail.js
+++ src/components/EntryDetail.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { connect } from 'react-redux'
-import { timeToString, getDailyReminderValue } from '../utils/helpers'
-import MetricCard from './MetricCard'
-import { white } from '../utils/colors'
-import TextButton from './TextButton'
-import { addEntry } from '../actions'
-import { removeEntry } from '../utils/api'
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import { timeToString, getDailyReminderValue } from "../utils/helpers";
+import MetricCard from "./MetricCard";
+import { white } from "../utils/colors";
+import TextButton from "./TextButton";
+import { addEntry } from "../actions";
+import { removeEntry } from "../utils/api";
 
 class EntryDetail extends Component {
-
   static navigationOptions = ({ navigation }) => {
-    const { entryId } = navigation.state.params
+    const { entryId } = navigation.state.params;
 
-    const year = entryId.slice(0, 4)
-    const month = entryId.slice(5,7)
-    const day = entryId.slice(8)
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
 
     return {
-      title: `${month}/${day}/${year}`
-    }
-  }
+      title: `${month}/${day}/${year}`,
+    };
+  };
 
   reset = () => {
-    const { remove, goBack, entryId } = this.props
+    const { remove, goBack, entryId } = this.props;
 
-    remove()
-    goBack()
-    removeEntry(entryId)
-  }
+    remove();
+    goBack();
+    removeEntry(entryId);
+  };
 
-  shouldComponentUpdate (nextProps) {
-    return nextProps.metrics !== null && !nextProps.metrics.today
+  shouldComponentUpdate(nextProps) {
+    return nextProps.metrics !== null && !nextProps.metrics.today;
   }
 
   render() {
-    const { metrics } = this.props
+    const { metrics } = this.props;
     return (
       <View style={styles.container}>
         <MetricCard metrics={metrics} />
-        <TextButton style={{margin: 20}} onPress={this.reset}>
+        <TextButton style={{ margin: 20 }} onPress={this.reset}>
           RESET
         </TextButton>
       </View>
@@ -53,31 +52,30 @@ const styles = StyleSheet.create({
     backgroundColor: white,
     padding: 15,
   },
-})
+});
 
-function mapStateToProps (state, { navigation }) {
-  const { entryId } = navigation.state.params
+function mapStateToProps(state, { navigation }) {
+  const { entryId } = navigation.state.params;
 
   return {
     entryId,
     metrics: state[entryId],
-  }
+  };
 }
 
-function mapDispatchToProps (dispatch, { navigation }) {
-  const { entryId } = navigation.state.params
+function mapDispatchToProps(dispatch, { navigation }) {
+  const { entryId } = navigation.state.params;
 
   return {
-    remove: () => dispatch(addEntry({
-      [entryId]: timeToString() === entryId
-        ? getDailyReminderValue()
-        : null
-    })),
+    remove: () =>
+      dispatch(
+        addEntry({
+          [entryId]:
+            timeToString() === entryId ? getDailyReminderValue() : null,
+        })
+      ),
     goBack: () => navigation.goBack(),
-  }
+  };
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(EntryDetail) 
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail);
--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,117 +1,137 @@
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-import { connect } from 'react-redux';
-import { sendbirdLogout, initMenu } from '../actions/menuActions';
+import React, { Component } from "react";
+import { View, Text, Button } from "react-native";
+import { connect } from "react-redux";
+import { sendbirdLogout, initMenu } from "../actions/menuActions";
 
 class Menu extends Component {
-    static navigationOptions = {
-        title: 'MENU'
-    }
+  static navigationOptions = {
+    title: "MENU",
+  };
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    UNSAFE_componentWillMount() {
-        this.props.initMenu();
-    }
+  UNSAFE_componentWillMount() {
+    this.props.initMenu();
+  }
 
-    UNSAFE_componentWillReceiveProps(props) {
-        const { isDisconnected } = props;
-        if (isDisconnected) {
-            // const resetAction = NavigationActions.reset({
-            //     index: 0,
-            //     actions: [
-            //         NavigationActions.navigate({ routeName: 'Login' })
-            //     ]
-            // })
-            // this.setState({ isLoading: false }, () => {
-            //     this.props.navigation.dispatch(resetAction);
-            // })
-            this.props.navigation.navigate('Login');
-        }
+  UNSAFE_componentWillReceiveProps(props) {
+    const { isDisconnected } = props;
+    if (isDisconnected) {
+      // const resetAction = NavigationActions.reset({
+      //     index: 0,
+      //     actions: [
+      //         NavigationActions.navigate({ routeName: 'Login' })
+      //     ]
+      // })e
+      // this.setState({ isLoading: false }, () => {
+      //     this.props.navigation.dispatch(resetAction);
+      // })
+      this.props.navigation.navigate("Login");
     }
+  }
 
-    _onProfileButtonPress = () => {
-        // TODO: Profile screen
-    }
+  _onProfileButtonPress = () => {
+    // TODO: Profile screen
+  };
 
-    _onOpenChannelPress = () => {
-        // TODO: OpenChannel screen
-        this.props.navigation.navigate('OpenChannel');
-    }
+  _onOpenChannelPress = () => {
+    // TODO: OpenChannel screen
+    this.props.navigation.navigate("OpenChannel");
+  };
 
-    _onGroupChannelPress = () => {
-        // TODO: GroupChannel screen
-    }
+  _onGroupChannelPress = () => {
+    // TODO: GroupChannel screen
+  };
 
-    _onDisconnectButtonPress = () => {
-        this.props.sendbirdLogout();
-    }
+  _onDisconnectButtonPress = () => {
+    this.props.sendbirdLogout();
+  };
 
-    render() {
-        return (
-            <View style={{backgroundColor: '#fff', flex: 1}}>
-                <Button
-                    containerViewStyle={styles.menuViewStyle}
-                    buttonStyle={styles.buttonStyle}
-                    backgroundColor='#fff'
-                    color='#6e5baa'
-                    icon={{name: 'user', type: 'font-awesome' , color: '#6e5baa', size: 16}}
-                    title='Profile'
-                    onPress={this._onProfileButtonPress}
-                />
-                <Button
-                    containerViewStyle={styles.menuViewStyle}
-                    buttonStyle={styles.buttonStyle}
-                    backgroundColor='#fff'
-                    color='#6e5baa'
-                    icon={{name: 'slack', type: 'font-awesome' , color: '#6e5baa', size: 16}}
-                    title='Open Channel' 
-                    onPress={this._onOpenChannelPress}
-                />
-                <Button
-                    containerViewStyle={styles.menuViewStyle}
-                    buttonStyle={styles.buttonStyle}
-                    backgroundColor='#fff'
-                    color='#6e5baa'
-                    icon={{name: 'users', type: 'font-awesome' , color: '#6e5baa', size: 16}}
-                    title='Group Channel' 
-                    onPress={this._onGroupChannelPress}
-                />
-                <Button
-                    containerViewStyle={styles.menuViewStyle}
-                    buttonStyle={styles.buttonStyle}
-                    backgroundColor='#fff'
-                    color='#7d62d9'
-                    color='#6e5baa'
-                    icon={{name: 'sign-out', type: 'font-awesome' , color: '#6e5baa', size: 16}}
-                    title='Disconnect' 
-                    onPress={this._onDisconnectButtonPress}
-                />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={{ backgroundColor: "#fff", flex: 1 }}>
+        <Button
+          containerViewStyle={styles.menuViewStyle}
+          buttonStyle={styles.buttonStyle}
+          backgroundColor="#fff"
+          color="#6e5baa"
+          icon={{
+            name: "user",
+            type: "font-awesome",
+            color: "#6e5baa",
+            size: 16,
+          }}
+          title="Profile"
+          onPress={this._onProfileButtonPress}
+        />
+        <Button
+          containerViewStyle={styles.menuViewStyle}
+          buttonStyle={styles.buttonStyle}
+          backgroundColor="#fff"
+          color="#6e5baa"
+          icon={{
+            name: "slack",
+            type: "font-awesome",
+            color: "#6e5baa",
+            size: 16,
+          }}
+          title="Open Channel"
+          onPress={this._onOpenChannelPress}
+        />
+        <Button
+          containerViewStyle={styles.menuViewStyle}
+          buttonStyle={styles.buttonStyle}
+          backgroundColor="#fff"
+          color="#6e5baa"
+          icon={{
+            name: "users",
+            type: "font-awesome",
+            color: "#6e5baa",
+            size: 16,
+          }}
+          title="Group Channel"
+          onPress={this._onGroupChannelPress}
+        />
+        <Button
+          containerViewStyle={styles.menuViewStyle}
+          buttonStyle={styles.buttonStyle}
+          backgroundColor="#fff"
+          color="#7d62d9"
+          color="#6e5baa"
+          icon={{
+            name: "sign-out",
+            type: "font-awesome",
+            color: "#6e5baa",
+            size: 16,
+          }}
+          title="Disconnect"
+          onPress={this._onDisconnectButtonPress}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = {
-    containerViewStyle: {
-        backgroundColor: '#fff', 
-        flex: 1
-    },
-    menuViewStyle: {
-        marginLeft: 0,
-        marginRight: 0
-    },
-    buttonStyle: {
-        justifyContent: 'flex-start',
-        paddingLeft: 14
-    }
+  containerViewStyle: {
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+  menuViewStyle: {
+    marginLeft: 0,
+    marginRight: 0,
+  },
+  buttonStyle: {
+    justifyContent: "flex-start",
+    paddingLeft: 14,
+  },
 };
 
 function mapStateToProps({ menu }) {
-    const { isDisconnected } = menu;
-    return { isDisconnected };
-};
+  const { isDisconnected } = menu;
+  return { isDisconnected };
+}
 
 export default connect(mapStateToProps, { sendbirdLogout, initMenu })(Menu);
