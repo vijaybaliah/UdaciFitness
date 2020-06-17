9e7db617c82dd8b4ad414be56ce895ed9d778b69
vijaybaliah <vijaybalu1989@gmail.com>
2020-06-18 02:30:38 +0530

    style: sendbird login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -6,89 +6,85 @@ import { sendbirdLogin } from '../actions/loginActions';
 import InputText from '../components/UI/InputText';
 
 class Login extends Component {
-    static navigationOptions = {
-        title: 'LOGIN'
-    }
+  static navigationOptions = {
+    title: 'LOGIN'
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            userId: '',
-            nickname: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: '',
+      nickname: ''
     }
+  }
 
-    UNSAFE_componentWillReceiveProps(props) {
-        const { user, error } = props;
-        if (user) {
-            // const resetAction = NavigationActions.reset({
-            //     index: 0,
-            //     actions: [
-            //         NavigationActions.navigate({ routeName: 'Menu' })
-            //     ]
-            // })
-            this.setState({ userId: '', nickname: '' })
-            this.props.navigation.navigate('Menu')
-        }
+  UNSAFE_componentWillReceiveProps(props) {
+    const { user, error } = props;
+    if (user) {
+      // const resetAction = NavigationActions.reset({
+      //     index: 0,
+      //     actions: [
+      //         NavigationActions.navigate({ routeName: 'Menu' })
+      //     ]
+      // })
+      this.setState({ userId: '', nickname: '' })
+      this.props.navigation.navigate('Menu')
     }
+  }
 
-    _userIdChanged = (userId) => {
-        this.setState({ userId });
-    }
+  _userIdChanged = (userId) => {
+    this.setState({ userId });
+  }
 
-    _nicknameChanged = (nickname) => {
-      console.log('nickname: ', nickname);
-        this.setState({ nickname });
-    }
+  _nicknameChanged = (nickname) => {
+    console.log('nickname: ', nickname);
+    this.setState({ nickname });
+  }
 
-    _onButtonPress = () => {
-        const { userId, nickname } = this.state;
-        this.props.sendbirdLogin({ userId, nickname });
-    }
+  _onButtonPress = () => {
+    const { userId, nickname } = this.state;
+    this.props.sendbirdLogin({ userId, nickname });
+  }
 
-    render() {
-        return (
-            <View style={{backgroundColor: '#fff', flex: 1}}>
-                <View style={styles.containerStyle}>
-                    <Text>User ID</Text>
-                    <InputText
-                      value={this.state.userId}
-                      onChange={(value) => this._userIdChanged(value)}
-                    />
-                </View>
-                <View style={styles.containerStyle}>
-                    <Text>Nickname</Text>
-                    <InputText
-                      value={this.state.nickname}
-                      onChange={this._nicknameChanged}
-                    />
-                </View>
-                <View style={styles.containerStyle}>
-                    <Button
-                        buttonStyle={{backgroundColor: '#2096f3'}}
-                        title='Connect' 
-                        onPress={this._onButtonPress}
-                    >
-                      {'Connect'}
-                    </Button>
-                </View>
-                <View style={styles.containerStyle}>
-                    <Text>{this.props.error}</Text>
-                </View>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={{ backgroundColor: '#fff', flex: 1 }}>
+        <Text>User ID</Text>
+        <InputText
+          value={this.state.userId}
+          onChange={(value) => this._userIdChanged(value)}
+        />
+        <Text>Nickname</Text>
+        <InputText
+          value={this.state.nickname}
+          onChange={this._nicknameChanged}
+        />
+        <View style={styles.containerStyle}>
+          <Button
+            buttonStyle={{ backgroundColor: '#2096f3' }}
+            title='Connect'
+            onPress={this._onButtonPress}
+          >
+            {'Connect'}
+          </Button>
+        </View>
+        <View style={styles.containerStyle}>
+          <Text>{this.props.error}</Text>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = {
-    containerStyle: {
-        marginTop: 10
-    }
+  containerStyle: {
+    marginTop: 10
+  }
 }
 
 function mapStateToProps({ sendbirdLogin }) {
-    const { error, user } = sendbirdLogin;
-    return { error, user };
+  const { error, user } = sendbirdLogin;
+  return { error, user };
 };
 
 export default connect(mapStateToProps, { sendbirdLogin })(Login);
